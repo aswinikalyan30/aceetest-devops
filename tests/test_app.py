4b51906cd0e3a5250aafebb3ee75a1201f09b4bc
Aswini Kalyan <ashwinikalyan30@gmail.com>
2026-03-10 09:54:23 +0530

    fix test failure

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
 import pytest
-from app import app
+import sys
+import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app
 
 @pytest.fixture
 def client():
